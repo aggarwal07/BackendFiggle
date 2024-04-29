076453a9604507347a963c2ec4d88ba3e9e94195
Aggarwal07 <shivamaggarwal179@gmail.com>
2024-04-29 17:41:18 +0530

    initial commit

---
+++ models/product.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: [String], // Assuming description is an array of strings
+        required: true,
+    },
+    images: {
+        type: [String], // Assuming images is an array of strings (URLs?)
+        required: true,
+    },
+    type: [{
+        size: {
+            type: String,
+            required: true,
+        },
+        material: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: String,
+            required: true,
+        },
+    }],
+    category: {
+        type: String,
+        required: true,
+    },
+});
+const Product = mongoose.model("Product", ProductSchema);
+module.exports = Product;
