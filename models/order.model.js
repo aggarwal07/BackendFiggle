04ac71849921c5687693a5141f96225a4a47e843
Aggarwal07 <shivamaggarwal179@gmail.com>
2024-05-23 17:17:20 +0530

    quantity in product added

--- models/order.model.js
+++ models/order.model.js
@@ -1,46 +1,47 @@
 const mongoose = require("mongoose");
 
 const OrderSchema = mongoose.Schema({
-    orderId :{
-        type: String,
-        required: false,
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    phoneNumber: {
-        type: String,
-        required: true,
+  orderId: {
+    type: String,
+    required: false,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  address: {
+    pinCode: {
+      type: String,
+      required: true,
     },
     address: {
-        pinCode: {
-            type: String,
-            required: true,
-        },
-        address: {
-            type: String,
-            required: true,
-        },
-        city: {
-            type: String,
-            required: true,
-        },
-        state: {
-            type: String,
-            required: true,
-        },
+      type: String,
+      required: true,
     },
-    cart: { type: Array, default: [] },
-    paymentMethod: {
-        type: String,
-        required: false,
-        enum: ['Credit Card', 'Debit Card', 'PayPal', 'Cash on Delivery'], // example payment methods
+    city: {
+      type: String,
+      required: true,
     },
-    orderDate: {
-        type: Date,
-        default: Date.now,
+    state: {
+      type: String,
+      required: true,
     },
+  },
+  cart: { type: Array, default: [] },
+  paymentMethod: {
+    type: String,
+    required: false,
+    enum: ["Credit Card", "Debit Card", "PayPal", "Cash on Delivery"],
+  },
+  orderDate: {
+    type: Date,
+    default: Date.now,
+  },
+  orderStatus: { type: String, required: false },
 });
 
 const Order = mongoose.model("Order", OrderSchema);
--- models/product.model.js
+++ models/product.model.js
@@ -43,6 +43,10 @@ const ProductSchema = mongoose.Schema({
     productType: {
         type: String,
         required: false,
+    },
+    quantity:{
+        type: number,
+        required: false,
     }
 });
 const Product = mongoose.model("Product", ProductSchema);
