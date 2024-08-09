4dd4be045fba33810323895f6220223cd60ed80a
Aggarwal07 <shivamaggarwal179@gmail.com>
2024-08-10 02:18:03 +0530

    canvify database updated

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ app.post('/api/orders',async (req,res) =>{
 })
 
 //database connection
-mongoose.connect("mongodb+srv://figgleCanvas:VfcW0greBHWeRmBg@ecomm.xcwucfo.mongodb.net/Figgle-Api?retryWrites=true&w=majority&appName=Ecomm").then(()=>{console.log("connection success")}).catch(()=>{console.log("connection failed")});
+mongoose.connect("mongodb+srv://canvifyy:w9F7vI2bvrcgsBkh@canvify.kcc83zj.mongodb.net/?retryWrites=true&w=majority&appName=Canvify").then(()=>{console.log("connection success")}).catch(()=>{console.log("connection failed")});
 
 //server
 app.listen(3000,()=>{
